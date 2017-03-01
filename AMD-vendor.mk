123b866aecc75857402f46fd19405e7f1c1987c4
abinyaazka <azkaabinya@gmail.com>
2017-03-02 03:13:06 +0700

    first commit

---
+++ AMD-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jandah/AMD/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jandah/AMD/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jandah/AMD/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jandah/AMD/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jandah/AMD/xbin,system/xbin)
+
